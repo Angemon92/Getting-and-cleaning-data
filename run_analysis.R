17c25d3e17312fba6a26e5fd9614e40772613de6
Stefan <Stefanpo92@gmail.com>
2014-05-25 00:25:55 +0200

    run_analysis.R modified. Separeted in getDataSet() and getAverage() functions.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+# getDataSet is function that returns tidy data that is decribed in first four instructions
+#(data set that contains only activity name, subject and mean and std columns)
+
 getDataSet <- function(){
         
 # MyRead for faster loading data set
@@ -10,72 +13,97 @@ getDataSet <- function(){
 # path of train and test dataSet        
         filesPathTrain = c("./train/X_train.txt","./train/y_train.txt","./train/subject_train.txt")                        
         filesPathTest = c("./test/X_test.txt","./test/y_test.txt","./test/subject_test.txt")
-       
+        
         print("loading large data tabe, please wait few seconds... ")
 # get X, y and subject both test and train
         X_test = myRead(filesPathTest[1])
         y_test = myRead(filesPathTest[2])
         subject_test = myRead(filesPathTest[3])
-
+        
         X_train = myRead(filesPathTrain[1])
         y_train = myRead(filesPathTrain[2])
         subject_train = myRead(filesPathTrain[3])
- 
+        
 # get features and activity_labels       
         features = myRead("./features.txt")
         activity_labels = myRead("./activity_labels.txt")
         
         features = as.character(features[,2])        
-       
+        
+# - 1) Merges the training and the test sets to create one data set. -
 # join test and train X       
         X = rbind(X_test, X_train)
-
+        
+# - 4) Appropriately labels the data set with descriptive activity names. -
 # add primary key for X named "rowNumber" ( for later merging with y)
 # name features ( columns )
         X = cbind(X, 1:10299)
         colnames(X) = c(features, "rowNumber")
-           
+        
 # join test and train y
         y = rbind(y_test,y_train)
-
+        
 # join test and train subject       
         subject = rbind(subject_test,subject_train)
-       
+        
 # add primary key for y named "rowNumber" ( for later merging with X)
         y = cbind(1:10299, y)
-
+# - 3) Uses descriptive activity names to name the activities in the data set. -
 # get y that contains: subject who is performing activity column, activity name column and primary key column 
         y = cbind(y, subject)
         colnames(y) = c("rowNumber","activityNumber","subject")
         colnames(activity_labels) = c("activityNumber", "activityName")
-       
+        
         y = merge(y, activity_labels, by = "activityNumber")
-        # drop activityNumber column
+# drop activityNumber column
         y = y[,2:4]
-
+        
 # merge X and y
         dataSet = merge(y, X, by = "rowNumber")
-
+        
 # remove primary key "rowNumber"
-         dataSet = dataSet[,2:564]
-
+        dataSet = dataSet[,2:564]
+        
 # reorder columns to: subject, ...features..., activityName
-         dataSet = subset(dataSet, select=c("subject", features, "activityName")) 
-
+        dataSet = subset(dataSet, select=c("subject", features, "activityName")) 
+        
+# - 2) Extracts only the measurements on the mean and standard deviation for each measurement. -
 # get only mean and std columns
-     
-         colNames = names(dataSet)
-         logicalMean = colNames %in% grep("mean", colNames, value = TRUE)
-         logicalStd = colNames %in% grep("std", colNames, value = TRUE)
-         logicalSubject = colNames %in% grep("subject", colNames, value = TRUE) 
-         logicalActivityName = colNames %in% grep("activityName", colNames, value = TRUE)
-         logicalRemoveFreq = !( colNames %in% grep("Freq", colNames, value = TRUE) )
-
-         logicalNeededColumns = (logicalMean | logicalStd | logicalSubject | logicalActivityName) & logicalRemoveFreq
-         #logicalNeededColumns = logical & logicalRemoveFreq
-
-         dataSet = dataSet[,logicalNeededColumns]
-
-         dataSet
-
+        
+        colNames = names(dataSet)
+        logicalMean = colNames %in% grep("mean", colNames, value = TRUE)
+        logicalStd = colNames %in% grep("std", colNames, value = TRUE)
+        logicalSubject = colNames %in% grep("subject", colNames, value = TRUE) 
+        logicalActivityName = colNames %in% grep("activityName", colNames, value = TRUE)
+        logicalRemoveFreq = !( colNames %in% grep("Freq", colNames, value = TRUE) )
+        
+        logicalNeededColumns = (logicalMean | logicalStd | logicalSubject | logicalActivityName) & logicalRemoveFreq
+#logicalNeededColumns = logical & logicalRemoveFreq
+        
+        dataSet = dataSet[,logicalNeededColumns]
+        
+        dataSet
 }
+
+
+# - 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. -
+#getAverage function returns data set that contains
+#average for each variable for each activity and each subject for input data set.
+
+getAverage <- function(data.frame){
+        
+        dataSet = data.frame
+        groupedData = split(dataSet, dataSet$subject)
+        # didnt had time to finish ...
+        
+}
+
+
+
+
+
+
+
+
+
+
